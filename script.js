8e408aa7100bedb51be1ee5ac4b6ceda2e70110a
Tarun Kumar <tpothineni@binghamton.edu>
2025-04-21 19:15:33 -0400

    Updated with latest project

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+=======
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+>>>>>>> 79c58d9 (Updated with latest project)
